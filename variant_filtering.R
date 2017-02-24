465f1a7802208a29f322af088d66be2d085eca1c
Phil9S <philsmith990@hotmail.co.uk>
2017-02-24 14:27:50 +0000

    file restore & markdown - script v2

--- variant_filtering.R
+++ variant_filtering.R
@@ -1,14 +1,14 @@
 rm(list = ls())
 args = commandArgs(trailingOnly=TRUE)
 ###set working directory and import arguments and libraries
-setwd(args[1])
+#setwd(args[1])
 require("stringr")
 clock <- as.character(Sys.time())
 
 
 ###default variables & log start
 write(clock, file = "R_log.txt", append = FALSE)
-write("##Variant Filter Script ## R-script Log - Log Begin", file = "R_log.txt", append = TRUE)
+write("##Variant Filter Script ## R-script Log - Log Begin (Version 2)", file = "R_log.txt", append = TRUE)
 
 
 ###Import data tables from bash script
@@ -34,6 +34,7 @@ write(varcount, file = "R_log.txt", append = TRUE)
 
 ###Add annotation cols to variant file
 vv$GENE <- anno$Gene.refGene
+vv$AA <- anno$AAChange.refGene
 vv$CONSEQUENCE <- anno$ExonicFunc.refGene
 vv$X1000G <- anno$X1000g2015aug_all
 vv$EXAC <- anno$ExAC_ALL
@@ -97,10 +98,23 @@ gthom <- data.frame(x=rep(0,nrow(vv.ft)))
 colnames(gthet) <- c("Het_percentage")
 colnames(gthom) <- c("Hom_percentage")
 gt <- gt[gt$ID %in% vv.ft$ID,]
+######################
+gtm <- as.matrix(gt[,-1])
+gtm[gtm == "0/0"] <- 0
+gtm[gtm == "0/1"] <- 1
+gtm[gtm == "1/1"] <- 2
+#gtm[gtm == "./."] <- NA
+gt <- as.data.frame(gtm)
+#######################
+refHOM2 <- apply(gtm,1, function(x) sum(x == 0))
+HET2 <- apply(gtm,1, function(x) sum(x == 1))
+nonHOM2 <- apply(gtm, 1, function(x) sum(x == 2))
+calc <- cbind(refHOM2,HET2,nonHOM2)
+
 for(r in 2:nrow(gt)){
-  refHOM <- as.numeric(sum(gt[r,2:ncol(gt)] == "0/0"))
-  HET <- as.numeric(sum(gt[r,2:ncol(gt)] == "0/1"))
-  nonHOM <- as.numeric(sum(gt[r,2:ncol(gt)] == "1/1"))
+  refHOM <- as.numeric(sum(gt[r,2:ncol(gt)] == 0))
+  HET <- as.numeric(sum(gt[r,2:ncol(gt)] == 1))
+  nonHOM <- as.numeric(sum(gt[r,2:ncol(gt)] == 2))
   hetpct <- (HET / (HET + nonHOM + refHOM))*100
   hompct <- (nonHOM / (HET + nonHOM + refHOM))*100
   gthet[r,1] <- hetpct
@@ -108,6 +122,26 @@ for(r in 2:nrow(gt)){
 }
 vv.ft$HET_rate <- gthet$Het_percentage
 vv.ft$HOM_rate <- gthom$Hom_percentage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###tidy variables and tables
 rm(HET,hetpct,nonHOM,refHOM,r,hompct)
 
@@ -138,7 +172,7 @@ for(i in 2:ncol(af)){
   d <- data.frame(x=rep(0,nrow(af)))
   ###loop over new maj/min read depth dataframe & convert to float or replace with na
   for(r in 1:nrow(alfe)){
-    if(alfe[r,2] == "."){d[r,1] <- NaN}
+    if(alfe[r,2] == "."){d[r,1] <- NA}
     else{
       afmaj <- as.numeric(alfe[r,1])
       afmin <- as.numeric(alfe[r,2])
@@ -160,7 +194,7 @@ for(i in 1:nrow(af)){
     if(max(af[i,2:ncol(af)], na.rm = TRUE) > 0.3){
         af.ft[i,1] <- af[i,1]}
     else{
-        af.ft[i,1] <- NaN
+        af.ft[i,1] <- NA
     }
 }
 names(af.ft)[1] <- "ID"
